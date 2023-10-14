a9b5e1330b7c2eeb0ec3a55cbe0f66660d2fb9e1
thiagoferreira <thiago.f.cerqueira@gmail.com>
2023-10-14 18:58:54 +0100

    !fix - Definir o endpoint final da aplicação.

--- app/src/main/java/br/pucminas/teamworktask/utils/Credentials.kt
+++ app/src/main/java/br/pucminas/teamworktask/utils/Credentials.kt
@@ -2,7 +2,6 @@ package br.pucminas.teamworktask.utils
 
 class Credentials {
     companion object {
-        var BASE_URL: String = "http://192.168.1.88:8080/"
-        //var BASE_URL: String = "https://teamworktaskservices.onrender.com/"
+        var BASE_URL: String = "https://teamworktaskservices.onrender.com/"
     }
 }
